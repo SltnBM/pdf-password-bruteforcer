ac178860f40f61971cb7058e8a52086632a75501
SltnBM <SltnBM@users.noreply.github.com>
2025-06-09 10:17:31 +0700

    feat: Add better error handling

--- pdf_bruteforcer.py
+++ pdf_bruteforcer.py
@@ -5,7 +5,20 @@ def brute_force_pdf(pdf_path, wordlist_path):
     try:
         with open(wordlist_path, 'r') as file:
             passwords = file.read().splitlines()
-
+    except FileNotFoundError:
+        print(f"[!] Wordlist file not found: {wordlist_path}")
+        return
+    
+    try:
+        with pikepdf.open(pdf_path):
+            print("[*] PDF is not password protected.")
+            return
+    except FileNotFoundError:
+        print(f"[!] PDF file not found: {pdf_path}")
+        return
+    except pikepdf._qpdf.PasswordError:
+        pass
+    
         for password in passwords:
             try:
                 with pikepdf.open(pdf_path, password=password):
